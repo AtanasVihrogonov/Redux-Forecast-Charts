8bef8e1f1c60209da338f2040ee6aaec221d4631
AV <vihrogonov@protonmail.com>
2021-06-28 14:31:24 +0100

    Create SearchBar & WeatherList Components

---
+++ src/containers/search_bar.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Go and fetch weather data
+    this.props.fetchWeather(this.state.term);
+    // after kick of clear the search input
+    this.setState({ term: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className='input-group'>
+        <input
+          placeholder='Get a five-day forecast in your favorite cities'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className='input-group-btn'>
+          <button type='submit' className='btn btn-secondary'>
+            Submit
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
+
+// hook up actionCreator(fetchWeather) to a search container
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
